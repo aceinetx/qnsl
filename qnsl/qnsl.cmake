679cc71deae590bd8ba650b391d86f923b3d58bb
aceinetx <mouselin82@gmail.com>
2025-03-16 14:37:27 +0300

    Update

--- qnsl/qnsl.cmake
+++ qnsl/qnsl.cmake
@@ -24,9 +24,9 @@ if(ANDROID)
 	file(GLOB_RECURSE PROJ_SOURCES CONFIGURE_DEPENDS "proj.android/*.cpp")
 	include_directories("proj.android")
 	
-	file(GLOB_RECURSE PDCURSES_SOURCES "qnsl/curses.linux/c++/*.cpp" "qnsl/curses.linux/form/*.c" "qnsl/curses.linux/ncurses/*.c" "qnsl/curses.linux/ncurses/base/*.c" "qnsl/curses.linux/ncurses/tinfo/*.c" "qnsl/curses.linux/ncurses/trace/*.c" "qnsl/curses.linux/ncurses/tty/*.c" "qnsl/curses.linux/ncurses/widechar/*.c" "qnsl/curses.linux/ncurses/*.c" "qnsl/curses.linux/panel/*.c" "qnsl/curses.linux/progs/*.c")
-	include("qnsl/curses.linux/include")
+	file(GLOB_RECURSE CURSES_SOURCES "qnsl/curses.linux/c++/*.cpp" "qnsl/curses.linux/form/*.c" "qnsl/curses.linux/ncurses/*.c" "qnsl/curses.linux/ncurses/base/*.c" "qnsl/curses.linux/ncurses/tinfo/*.c" "qnsl/curses.linux/ncurses/trace/*.c" "qnsl/curses.linux/ncurses/tty/*.c" "qnsl/curses.linux/ncurses/widechar/*.c" "qnsl/curses.linux/ncurses/*.c" "qnsl/curses.linux/panel/*.c" "qnsl/curses.linux/progs/*.c")
+  include_directories("qnsl/curses.linux/ncurses")
 
-	add_executable(${APP_NAME} ${PROJ_SOURCES} ${QNSL_SOURCES} ${APP_SOURCES})
+	add_executable(${APP_NAME} ${PROJ_SOURCES} ${QNSL_SOURCES} ${APP_SOURCES} ${CURSES_SOURCES})
 	target_link_libraries(${APP_NAME})
 endif()
